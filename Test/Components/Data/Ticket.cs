9ed4e10c8453ba06f628112fedfd526ad05be539
Playos0 <krzysiek.1.k@wp.pl>
2025-05-29 17:35:41 +0200

    Some changes
    -Dodano Do navBar wszystkie podstrony
    -Dodano Przyciski zmiany statusu w TicketList

--- Test/Components/Data/Ticket.cs
+++ Test/Components/Data/Ticket.cs
@@ -1,8 +1,14 @@
 ﻿//Klasa definiująca zgłoszenie.
+using System.ComponentModel.DataAnnotations;
 public class Ticket
 {
+    [Required(ErrorMessage = "Imię klienta jest wymagane!")]
     public string ClientName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Nazwa urządzenia jest wymagana!")]
     public string Device { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Opis jest wymagany!")]
     public string Description { get; set; } = string.Empty;
     public string Status { get; set; } = "Nowe";
 }
